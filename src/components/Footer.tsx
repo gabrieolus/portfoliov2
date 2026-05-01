81bc3f903e158fe299bb18bf92cc0c1addb2ae8b
gabrieolus <55069587+gabrieolus@users.noreply.github.com>
2026-05-01 15:47:10 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   
   return (
-    <footer className="py-20 border-t hextech-border w-full">
-      <div className="max-w-[1720px] mx-auto px-6 text-center">
+    <footer className="py-20 border-t w-full">
+      <div className="w-full px-6 text-center">
         <div className="flex justify-center mb-20">
           <div className="flex gap-4">
             {[
